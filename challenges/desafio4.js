836526dc9fbc09eb6252444eaf897424bb3f715b
Dayana <dayanadiasgarcia@gmail.com>
2022-07-25 18:11:13 -0300

    Requisito 04

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,1 +1,8 @@
 // 4 - Retorne os lanches que tiveram vendas maiores que 50 e menores que 100, mostrando apenas o nome e a quantidade de lanches vendidos em ordem crescente
+db.produtos.find({
+  vendidos: { $gt: 50, $lt: 100}
+}, {
+  nome: true, vendidos: true, _id: false
+}).sort({
+  vendidos: 1
+});
