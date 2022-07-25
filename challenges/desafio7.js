b7e0acbf160c81c8d037d081f82adc1215aac25a
Dayana <dayanadiasgarcia@gmail.com>
2022-07-25 18:57:36 -0300

    Requisito 07

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,1 +1,9 @@
 // 7 - Retorne o nome e vendidos dos lanches que tenham sido vendidos com uma quantidade diferente de 50 e em que o campo tags não exista
+
+db.produtos.find({
+  $nor: [
+    { vendidos: 50 },
+    { tags: { $exists: true } }]
+}, {
+  nome: true, vendidos: true, _id: false
+});
