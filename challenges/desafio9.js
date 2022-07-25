8925eac36d6f7ad8182786537ed2e1f615f422a6
Dayana <dayanadiasgarcia@gmail.com>
2022-07-25 19:46:31 -0300

    Requisito 09

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,1 +1,7 @@
 // 9 - Retorne o nome de todos os lanches que possuam calorias abaixo de 500
+db.produtos.find({
+  valoresNutricionais: { $elemMatch: {
+    tipo: "calorias",
+    quantidade: {$lt: 500 }
+  }
+}}, { nome: true, _id: false });
