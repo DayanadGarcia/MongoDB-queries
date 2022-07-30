c7ce95a78a5f88107def6388ee20fcd152f8eb22
Dayana <dayanadiasgarcia@gmail.com>
2022-07-30 01:34:02 -0300

    Requisito 25

---
+++ challenges/desafio25.js
@@ -0,0 +1,19 @@
+// 25 - Adicione o valor muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40
+db.produtos.updateMany({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: "sódio",
+      percentual: {
+        $gte: 40,
+      },
+    }, 
+  },
+}, {
+  $push: {
+    tags: "muito sódio",
+  },
+});
+
+db.produtos.find({}, {
+  nome: true, tags: true, _id: false,
+});
